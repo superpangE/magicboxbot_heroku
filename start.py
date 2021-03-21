5a714eb442197256e5bf21f953dd327015195927
pangE <cdpkct@gmail.com>
2021-03-21 18:19:14 +0900

    [feat] 음악 기능 추가

--- start.py
+++ start.py
@@ -1,6 +1,15 @@
-import pig
 import discord
+from discord.utils import get
+import asyncio
+import random
+import time
+import urllib.request
+from urllib.error import URLError, HTTPError
+from bs4 import BeautifulSoup
 import os
+import youtube_dl
+from youtube_dl import YoutubeDL
+from discord import FFmpegPCMAudio
 from discord.ext import commands
 client = discord.Client()
 bot =commands.Bot(command_prefix="!")
